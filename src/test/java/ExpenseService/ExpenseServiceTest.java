3380f4d8c0abb4ef78fed8aff674ed1f72763bdd
JarryLi <3244188372@qq.com>
2019-07-15 20:28:02 +0800

    test:use mock test other_expense_type

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -6,7 +6,6 @@ import ExpenseService.Project.Project;
 import ExpenseService.Project.ProjectType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -41,15 +40,27 @@ class ExpenseServiceTest {
     @Test
     void should_return_expense_type_B_if_project_is_external_and_name_is_project_B() throws UnexpectedProjectTypeException {
         // given
+        Project project = mock(Project.class);
+        given(project.getProjectType()).willReturn(ProjectType.EXTERNAL);
+        given(project.getProjectName()).willReturn("Project B");
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+        verify(project,times(1));
+        assertSame(expenseType,ExpenseType.EXPENSE_TYPE_B);
     }
 
     @Test
     void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
         // given
+        Project project = mock(Project.class);
+        given(project.getProjectType()).willReturn(ProjectType.EXTERNAL);
+        given(project.getProjectName()).willReturn("jerryLi");
         // when
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+        verify(project,times(1));
+        assertSame(expenseType,ExpenseType.OTHER_EXPENSE);
     }
 
     @Test
